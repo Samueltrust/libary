573eb972dfcd911c63ca3e6903f272bd265d9e90
Samueltrust <135181817+Samueltrust@users.noreply.github.com>
2023-11-01 14:54:18 +0000

    Update index.js

--- index.js
+++ index.js
@@ -4,39 +4,121 @@ let scissors = document.querySelector('.scissors');
 let result = document.querySelector('.result');
 let inputs = document.querySelector('.inputs');
 let scores = document.querySelector('.scores');
+let reset = document.querySelector('.reset');
 
 
+let wins = 0;
+let loss = 0;
+let tie = 0;
+let computerImage;
+let userImage;
+const Images = ['rock-emoji.png', 'paper-emoji.png', 'scissors-emoji.png']
 
-let randomNumber = Math.floor(Math.random() * 3) + 1;
-let computerMove;
+rock.addEventListener('click', function() {
+    let chosen = 'rock';
+    calculation(chosen);   
+});
 
-if (randomNumber === 1) {
-    computerMove = rock;
-} else if (randomNumber === 2) {
-    computerMove = paper;
-} else if (randomNumber === 3) {
-    computerMove = scissors
+
+paper.addEventListener('click', function() {
+    let chosen = 'paper';
+    calculation(chosen);
+});
+
+
+scissors.addEventListener('click', function() {
+    let chosen = 'scissors';
+    calculation(chosen);
+});
+
+
+reset.addEventListener('click', function(){
+    resetScore();
+});
+
+window.onload = function () {
+    if (localStorage.getItem('wins')) {
+        wins = parseInt(localStorage.getItem('wins'));
+        loss = parseInt(localStorage.getItem('loss'));
+        tie = parseInt(localStorage.getItem('tie'));
+        updateScore();
+    }
+};
+
+function updateScore() {
+    scores.textContent = `Wins: ${wins}, Losses: ${loss}, Ties: ${tie}`;
+    localStorage.setItem('wins', wins);
+    localStorage.setItem('loss', loss);
+    localStorage.setItem('tie', tie);
 }
 
 function calculation(value) {
-    let wins = 0;
-    let loss = 0;
-    let tie = 0;
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    let computerMove;
 
-    if (computerMove === rock && value === rock || computerMove === paper && value === paper || computerMove === scissors && value === scissors) {
+
+    if (randomNumber === 1) {
+        computerMove = 'rock';
+        computerImage = Images[0];
+    } else if (randomNumber === 2) {
+        computerMove = 'paper';
+        computerImage = Images[1];
+    } else if (randomNumber === 3) {
+        computerMove = 'scissors';
+        computerImage = Images [2];
+    }
+    
+
+    if (value === 'rock') {
+        userImage = Images[0];
+    }
+
+    if (value === 'paper') {
+        userImage = Images[1];
+    }
+
+    if (value === 'scissors') {
+        userImage = Images[2];
+    }
+
+    if (
+       (computerMove === 'rock' && value === 'scissors') ||
+       (computerMove === 'paper' && value === 'rock') || 
+       (computerMove === 'scissors' && value === 'paper')
+     ) {
+        loss += 1;
+        result.textContent = "You Lose.";
+    } else if (
+       (computerMove === 'rock' && value === 'paper') ||
+       (computerMove === 'paper' && value === 'scissors') || 
+       (computerMove === 'scissors' && value === 'rock') 
+     ) {
+        wins += 1;
+        result.textContent = "You win.";
+    } else {
         tie += 1;
         result.textContent = "Tie.";
-        inputs.textContent = `You ${value}  ${computerMove} Computer`;
-        scores.textContent = `Wins: ${wins}, Losses: ${loss} Ties: ${tie}`;
-    } 
-
-
-    if (computerMove === rock && value === scissors || computerMove === paper && value === rock || computerMove === scissors && value === paper) {
-        loss -= 1;
-    } 
-    
-    
-    if (computerMove === rock && value === paper || computerMove === paper && value === scissors || computerMove === scissors && value === rock) {
-        wins += 1;
+        
     }
+
+    inputs.innerHTML = `You <img src="${userImage}" alt="Your choice"> <img src="${computerImage}" alt="Computer choice"> Computer`;
+    scores.textContent = `Wins: ${wins}, Losses: ${loss} Ties: ${tie}`;
+
+    updateScore();
+}
+
+function resetScore() {
+    wins = 0;
+    loss = 0;
+    tie = 0;
+
+    updateScore();
+    localStorage.removeItem('wins');
+    localStorage.removeItem('loss');
+    localStorage.removeItem('tie');
+
+
+    result.textContent = "";
+    inputs.textContent = "";
+    scores.textContent = "";
 }
